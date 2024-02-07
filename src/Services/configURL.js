f6f5c3dbbb2edc7368e17173225a1b42171e62cd
Pham Duc Dat <phamducdat@Dat-Phams-MacBook-Pro.local>
2024-02-08 00:27:52 +0700

    push

--- src/Services/configURL.js
+++ src/Services/configURL.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { localServ } from "./localService";
 export const BASE_URL = "https://airbnbnew.cybersoft.edu.vn";
 export const TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA1NCIsIkhldEhhblN0cmluZyI6IjIyLzA1LzIwMjQiLCJIZXRIYW5UaW1lIjoiMTcxNjMzNjAwMDAwMCIsIm5iZiI6MTY4NzcxMjQwMCwiZXhwIjoxNzE2NDgzNjAwfQ.argi0m1LRAePDxZ6Nb4AX25fZ9gclDCUAA5oW84-TsQ";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJOb2RlSlMgMzciLCJIZXRIYW5TdHJpbmciOiIzMS8xMi8yMTAwIiwiSGV0SGFuVGltZSI6IjQxMzM4OTQ0MDAwMDAiLCJuYmYiOjE2OTg1MTI0MDAsImV4cCI6MTcxNjY1NjQwMH0.nJ0FPo5wkc1WgelxELfkD3BJ8WrFfjXNH_EGwFMsWh8";
 export let https = axios.create({
   baseURL: BASE_URL,
   headers: {
